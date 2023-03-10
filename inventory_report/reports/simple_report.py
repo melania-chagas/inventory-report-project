cb9afaabcb22ed5d16b0c4154e343d9fe41ef6f9
Melania P Chagas <melaniapchagas@gmail.com>
2023-03-10 19:14:40 -0300

    refactor: refatoração e comentários na classe 'SimpleReport'

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -4,36 +4,52 @@ from datetime import date
 
 
 class SimpleReport:
-    @classmethod
-    def generate(cls, dict_list):
+    def get_oldest_manufacture(dict_list):
         manufacture_list = []
-        validity_list = []
-        list_of_companies = []
 
         for item in dict_list:
             manufacture_list.append(item["data_de_fabricacao"])
-            validity_list.append(item["data_de_validade"])
-            list_of_companies.append(item["nome_da_empresa"])
 
         manufacture_list.sort()
-        unexpired_dates_in_validity_list = []
+        return manufacture_list[0]
+
+    def get_next_due_date(dict_list):
+        expiration_date = []
+
+        for item in dict_list:
+            expiration_date.append(item["data_de_validade"])
+
+        unexpired_date_list = []
         today = date.today()
-        for date_ in validity_list:
+        for date_ in expiration_date:
             if date_ > str(today):
-                unexpired_dates_in_validity_list.append(date_)
-        unexpired_dates_in_validity_list.sort()
+                unexpired_date_list.append(date_)
+        unexpired_date_list.sort()
 
-        oldest_manufacture = manufacture_list[0]
-        next_due_date = unexpired_dates_in_validity_list[0]
-        # https://docs.python.org/pt-br/3/library/collections.html
+        return unexpired_date_list[0]
+
+    def get_greater_quantity_of_products(dict_list):
+        list_of_companies = []
+
+        for item in dict_list:
+            list_of_companies.append(item["nome_da_empresa"])
+
         result = Counter(list_of_companies)
-        print(result.most_common(1)[0])
-        greater_quantity_of_products = result.most_common(1)[0][0]
 
+        # 'result.most_common(1)' retorna: [('Target Corporation', 4)]
+        # 'result.most_common(1)[0]' retorna: ('Target Corporation', 4)
+        # 'result.most_common(1)[0][0]' retorna: Target Corporation
+        return result.most_common(1)[0][0]
+
+    @classmethod
+    def generate(cls, dict_list):
         return (
-            f"Data de fabricação mais antiga: {oldest_manufacture}\n"
-            f"Data de validade mais próxima: {next_due_date}\n"
-            f"Empresa com mais produtos: {greater_quantity_of_products}"
+            "Data de fabricação mais antiga: "
+            f"{cls.get_oldest_manufacture(dict_list)}\n"
+            "Data de validade mais próxima: "
+            f"{cls.get_next_due_date(dict_list)}\n"
+            f"Empresa com mais produtos: "
+            f"{cls.get_greater_quantity_of_products(dict_list)}"
         )
 
 
