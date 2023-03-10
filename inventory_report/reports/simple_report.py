e995cdbe4fbb854044ef9662de52f1a551be8157
Melania P Chagas <melaniapchagas@gmail.com>
2023-03-10 09:54:10 -0300

    feat: Implementando método 'generate' na classe 'SimpleReport'

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,38 @@
+import json
+import collections
+
+
 class SimpleReport:
-    pass
+    @classmethod
+    def generate(cls, dict_list):
+        manufacture_list = []
+        validity_list = []
+        list_of_companies = []
+
+        for item in dict_list:
+            manufacture_list.append(item["data_de_fabricacao"])
+            validity_list.append(item["data_de_validade"])
+            list_of_companies.append(item["nome_da_empresa"])
+
+        manufacture_list.sort()
+        validity_list.sort()
+
+        oldest_manufacture = manufacture_list[0]
+        next_due_date = validity_list[0]
+        # https://docs.python.org/pt-br/3/library/collections.html
+        counter = collections.Counter()
+        for company in list_of_companies:
+            counter[company] += 1
+        greater_quantity_of_products = list(counter.keys())[0]
+
+        return (
+            f"Data de fabricação mais antiga: {oldest_manufacture}\n"
+            f"Data de validade mais próxima: {next_due_date}\n"
+            f"Empresa com mais produtos: {greater_quantity_of_products}\n"
+        )
+
+
+with open("inventory_report/data/inventory.json", encoding='utf-8') as json_:
+    data = json.load(json_)
+
+print(SimpleReport.generate(data))
