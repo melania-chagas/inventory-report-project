d064b712cd1cc52a8f57a1debf2385778ad554e2
Melania P Chagas <melaniapchagas@gmail.com>
2023-03-10 12:06:52 -0300

    feat: implementando lógica que seleciona datas não vencidas

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,5 +1,6 @@
 # import json
-import collections
+from collections import Counter
+from datetime import date
 
 
 class SimpleReport:
@@ -15,20 +16,24 @@ class SimpleReport:
             list_of_companies.append(item["nome_da_empresa"])
 
         manufacture_list.sort()
-        validity_list.sort()
+        unexpired_dates_in_validity_list = []
+        today = date.today()
+        for date_ in validity_list:
+            if date_ > str(today):
+                unexpired_dates_in_validity_list.append(date_)
+        unexpired_dates_in_validity_list.sort()
 
         oldest_manufacture = manufacture_list[0]
-        next_due_date = validity_list[0]
+        next_due_date = unexpired_dates_in_validity_list[0]
         # https://docs.python.org/pt-br/3/library/collections.html
-        counter = collections.Counter()
-        for company in list_of_companies:
-            counter[company] += 1
-        greater_quantity_of_products = list(counter.keys())[0]
+        result = Counter(list_of_companies)
+        print(result.most_common(1)[0])
+        greater_quantity_of_products = result.most_common(1)[0][0]
 
         return (
             f"Data de fabricação mais antiga: {oldest_manufacture}\n"
             f"Data de validade mais próxima: {next_due_date}\n"
-            f"Empresa com mais produtos: {greater_quantity_of_products}\n"
+            f"Empresa com mais produtos: {greater_quantity_of_products}"
         )
 
 
